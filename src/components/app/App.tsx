767af15d7384f1c4dd802cbacf2e1e9a50eeb36c
Maslomeister <maslomeister@gmail.com>
2022-07-26 16:35:30 +0300

    refactor: cleanup

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useMemo } from "react";
 import { v4 as uuid } from "uuid";
 
 import { NewTask } from "../new-task/new-task";
--- src/services/reducers/todosSlice.ts
+++ src/services/reducers/todosSlice.ts
@@ -1,5 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { RootState, AppThunk } from "../store";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface ITodosState {
   tasks: ITask[];
@@ -40,21 +39,5 @@ export const todosSlice = createSlice({
 });
 
 export const { addTask, changeTaskState, clearCompleted } = todosSlice.actions;
-
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-//export const selectCount = (state: RootState) => state.counter.value;
-
-// We can also write thunks by hand, which may contain both sync and async logic.
-// Here's an example of conditionally dispatching actions based on current state.
-// export const incrementIfOdd =
-//   (amount: number): AppThunk =>
-//   (dispatch, getState) => {
-//     const currentValue = selectCount(getState());
-//     if (currentValue % 2 === 1) {
-//       dispatch(incrementByAmount(amount));
-//     }
-//   };
 
 export default todosSlice.reducer;
